b268e02c76914bee2ed41c522cf17bf826ccf667
Kvnjet <kvnjet@gmail.com>
2023-09-03 13:23:16 -0600

    Se sube codigo de planta.java
    Se sube codigo de planta.java

--- Caso_1/src/Patio/Planta.java
+++ Caso_1/src/Patio/Planta.java
@@ -1,17 +1,23 @@
 package Patio;
+import Soporte.Util;
+import java.awt.Color;
 
 public class Planta {
 	
-	private String Tipo;
-	private int Humedad;
-	private int Cantidad;
+	String[] Tipo = {"Margarita", "Rosa", "Cactus", "Diente de león"};
+	int Humedad;
+	int Cantidad;
+	int Flores;
+	int Frutos;
 	
 	public void florecer() {
-	
+		Flores =+ (int)(Math.random()*5)
+		Util.report("La planta ha florecido", Color.green, "Patio", 10, true);
 	}
 	
 	public void darFruto() {
-		
+		Frutos =+ (int)(Math.random()*5)
+		Util.report("La planta ha producido frutos", Color.cyan, "Patio", 10, true);
 	}
 	
 	
